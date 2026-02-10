42dbf8a231ef136c50baa5ef84ce5ed09701cfad
japrapha2546-source <japrapha2546@gmail.com>
2026-02-10 11:10:30 +0530

    Create Sort_the_People.c

---
+++ Sort_the_People.c
@@ -0,0 +1,16 @@
+char** sortPeople(char** names,int namesSize,int* heights,int heightsSize,int* returnSize){
+    for(int i=0;i<namesSize;i++){
+        for(int j=i+1;j<namesSize;j++){
+            if(heights[i]<heights[j]){
+               int h=heights[i];
+               heights[i]=heights[j];
+               heights[j]=h;
+               char* n=names[i];
+               names[i]=names[j];
+               names[j]=n;
+            }
+        }
+    }
+    *returnSize=namesSize;
+    return names;
+}
