6f0478b583e3e0f7ff14820c1d6e81b095098e61
japrapha2546-source <japrapha2546@gmail.com>
2026-02-12 10:19:47 +0530

    Create Maximum_Number_Of_Vowels_In_a_Substring.c

---
+++ Maximum_Number_Of_Vowels_In_a_Substring.c
@@ -0,0 +1,34 @@
+Example 1:
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+
+Example 2:
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isVowel(char c){
+    return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+}
+int maxVowels(char*s,int k){
+    int n=strlen(s);
+    int count=0;
+    int maxCount=0;
+    for(int i=0;i<k&&i<n;i++){
+        if(isVowel(s[i]))
+            count++;
+    }
+    maxCount=count;
+    for(int i=k;i<n;i++){
+        if(isVowel(s[i-k]))count--;
+        if(isVowel(s[i]))count++;
+        if(count>maxCount)
+            maxCount=count;
+    }
+    return maxCount;
+}
