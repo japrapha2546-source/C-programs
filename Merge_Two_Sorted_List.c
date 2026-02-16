5ccfc29ff77a3a79d3dabfc5b4d86e21d6f08681
japrapha2546-source <japrapha2546@gmail.com>
2026-02-16 21:51:11 +0530

    Function to merge two sorted linked lists
    Implement mergeTwoLists to combine two sorted linked lists into one.

---
+++ Merge_Two_Sorted_List.c
@@ -0,0 +1,25 @@
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    struct ListNode dummy;
+    struct ListNode* tail=&dummy;
+    dummy.next=NULL;
+    while(list1&&list2){
+        if(list1->val<=list2->val){
+            tail->next=list1;
+            list1=list1->next;
+        }
+        else{
+            tail->next=list2;
+            list2=list2->next;
+        }
+        tail=tail->next;
+    }
+    if(list1) 
+     tail->next=list1;
+    else 
+     tail->next=list2;
+    return dummy.next;
+}
