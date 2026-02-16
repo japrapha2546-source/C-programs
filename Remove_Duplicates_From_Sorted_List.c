f7e0b96430d6fd8166d16349d187f9ea24c240ac
japrapha2546-source <japrapha2546@gmail.com>
2026-02-16 22:09:43 +0530

    Remove_Duplicates_From_Sorted_List.c
    Implement function to remove duplicates from a sorted linked list.

---
+++ Remove_Duplicates_From_Sorted_List.c
@@ -0,0 +1,16 @@
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+
+
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    struct ListNode* curr=head;
+    while(curr && curr->next){
+        if(curr->val==curr->next->val)
+            curr->next=curr->next->next;
+        else
+            curr=curr->next;
+    }
+    return head;
+}
