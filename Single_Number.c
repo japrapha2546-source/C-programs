c412a42a22f87f8596604d5597fc41b90dd74c5a
japrapha2546-source <japrapha2546@gmail.com>
2026-02-18 23:01:07 +0530

    Single_Number.c
    Implement function to find single number in array

---
+++ Single_Number.c
@@ -0,0 +1,21 @@
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+
+int singleNumber(int* nums, int numsSize) {
+    int result = 0;
+    for (int i = 0; i < numsSize; i++) {
+        result ^= nums[i];
+    }
+    return result;
+}
+
+ 
