4d18c6da7adb06b96193c3066e8d64a23ccf69f3
japrapha2546-source <japrapha2546@gmail.com>
2026-02-18 22:49:36 +0530

    Same_Tree.c
    Implement function to check if two binary trees are the same.

---
+++ Same_Tree.c
@@ -0,0 +1,17 @@
+Example 1:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (!p && !q)
+        return true;
+    if (!p || !q)
+        return false;
+    if (p->val != q->val)
+        return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
