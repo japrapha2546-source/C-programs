1470dd38d5ed300f04f591b472984386b6f27628
japrapha2546-source <japrapha2546@gmail.com>
2026-02-19 09:39:35 +0530

    Array_Nesting.c

--- Array_Nesting.c
+++ Array_Nesting.c
@@ -8,7 +8,8 @@ int arrayNesting(int* nums,int n){
             start=next;
             count++;
         }
-        if(count>maxlen) maxlen=count;
+        if(count>maxlen)
+            maxlen=count;
     }
     return maxlen;
 }
