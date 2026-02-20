55ef137c92fe264ac37086bb41d82a9110eafd60
japrapha2546-source <japrapha2546@gmail.com>
2026-02-20 23:06:00 +0530

    reverse-integer.c

--- 7-reverse-integer/reverse-integer.c
+++ 7-reverse-integer/reverse-integer.c
@@ -1,7 +1,8 @@
 int reverse(int x){
     int rev=0;
     while(x){
-        if(rev>214748364 || rev<-214748364) return 0; // overflow check
+        if(rev>214748364 || rev<-214748364)
+            return 0; 
         rev = rev*10 + x%10;
         x /= 10;
     }
