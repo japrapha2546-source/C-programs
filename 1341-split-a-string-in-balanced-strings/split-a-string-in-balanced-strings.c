4d236e8acc48d08e6cf8a6a205e3366ae7837325
japrapha2546-source <japrapha2546@gmail.com>
2026-03-02 22:24:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (2.94%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.c
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.c
@@ -1,13 +1,13 @@
-int balancedStringSplit(char* s) {
-    int balance = 0;
-    int count = 0;
-    for (int i = 0; s[i] != '\0'; i++) {
-        if (s[i] == 'R')
+int balancedStringSplit(char* s){
+    int balance=0;
+    int count=0;
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]=='R')
             balance++;
         else
             balance--;
 
-        if (balance == 0)
+        if(balance==0)
             count++;
     }
     return count;
