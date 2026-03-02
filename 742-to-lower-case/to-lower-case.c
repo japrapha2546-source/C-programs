9dacb7f6d9f312d8fb3795e64417f1d7f8b12120
japrapha2546-source <japrapha2546@gmail.com>
2026-03-02 22:26:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (1.55%) - LeetSync

--- 742-to-lower-case/to-lower-case.c
+++ 742-to-lower-case/to-lower-case.c
@@ -1,7 +1,7 @@
-char* toLowerCase(char* s) {
-    for (int i = 0; s[i] != '\0'; i++) {
-        if (s[i] >= 'A' && s[i] <= 'Z')
-            s[i] = s[i] + 32;
+char* toLowerCase(char* s){
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]>='A' && s[i]<='Z')
+            s[i]=s[i]+32;
     }
     return s;
 }
