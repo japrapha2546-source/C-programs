52db0c4a2c3ff35ead01cf32c39c89886ee7ee41
japrapha2546-source <japrapha2546@gmail.com>
2026-03-04 14:35:03 +0530

    Update Valid_Anagram.c

--- Valid_Anagram.c
+++ Valid_Anagram.c
@@ -1,15 +1,17 @@
-#define SIZE 256
 bool isAnagram(char* s, char* t) {
-    int count[SIZE] = {0};
-
-    for(int i = 0; s[i] != '\0'; i++)
-     count[tolower(s[i])]++;
-    for(int i = 0; t[i] != '\0'; i++) 
-     count[tolower(t[i])]--;
-
-    for(int i = 0; i < SIZE; i++) {
-        if(count[i] != 0) 
-          return false;
+    if(strlen(s)!=strlen(t))
+        return false;
+    int count[26]={0};
+    
+    for(int i=0;s[i]!='\0';i++)
+        count[s[i]-'a']++;
+    
+    for(int i=0;t[i]!='\0';i++)
+        count[t[i]-'a']--;
+    
+    for(int i=0;i<26;i++){
+        if(count[i]!=0)
+           return false;
     }
     return true;
 }
