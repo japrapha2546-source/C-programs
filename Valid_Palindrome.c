a23997e8db9801cb74dd8176e9bc79913f88d51a
japrapha2546-source <japrapha2546@gmail.com>
2026-03-04 14:26:21 +0530

    125. Valid_Palindrome.c

--- Valid_Palindrome.c
+++ Valid_Palindrome.c
@@ -1,16 +1,19 @@
 bool isPalindrome(char* s) {
-    int left = 0;
-    int right = strlen(s) - 1;
-
-    while (left < right) {
-        while (left < right && !isalnum(s[left])) 
-            left++;
-        while (left < right && !isalnum(s[right])) 
-            right--;
-        if (tolower(s[left]) != tolower(s[right]))
-           return false;
-        left++;
-        right--;
+    int l=0;
+    int r=strlen(s)-1;
+    while(l<r){
+        if(!isalnum(s[l])){
+            l++;
+            continue;
+        }
+        if(!isalnum(s[r])){
+            r--;
+            continue;
+        }
+        if(tolower(s[l])!=tolower(s[r]))
+            return false;
+        l++;
+        r--;
     }
     return true;
 }
