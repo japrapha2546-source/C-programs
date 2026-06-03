a0c647b3a0abd569a398229146103e2347d560fa
japrapha2546-source <japrapha2546@gmail.com>
2026-06-03 09:39:45 +0530

    Linear_search.c

--- Linear_search.c
+++ Linear_search.c
@@ -2,28 +2,34 @@
 
 int linearSearch(int arr[],int n,int target){
     for(int i=0;i<n;i++){
-        if(arr[i]==target){
+        if(arr[i]==target)
             return i;
-        }
     }
     return -1;
 }
+
 int main(){
     int n,target;
+
+    printf("Enter number of elements: ");
     scanf("%d",&n);
+
     int arr[n];
+
+    printf("Enter array elements:\n");
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
+
+    printf("Enter target value: ");
     scanf("%d",&target);
 
     int result=linearSearch(arr,n,target);
 
     if(result!=-1)
-        printf("%d",result);
+        printf("Element found at index: %d\n",result);
     else
-        printf("-1");
+        printf("Element not found\n");
 
     return 0;
 }
-
